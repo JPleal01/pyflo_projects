db759362eab1082870e281d75e2710e53d51b3a6
Joao Paulo Leal <103160122+JPleal01@users.noreply.github.com>
2025-09-24 11:11:52 -0300

    Create guided_project_3.py

---
+++ guided_project_3.py
@@ -0,0 +1,41 @@
+# First task
+
+def print_head(hair, eye):
+    hair = hair
+    eye = eye
+    full_hair = hair*12
+    
+    print(full_hair)
+    print(hair + '|        |'+ hair)
+    print(hair + '|  ' + eye + '  ' + eye + '  |' + hair)
+    print(' |   /\   |' )
+    print(' |        |' )
+    print(' \  \'--\'  /')
+    print('   ------')
+
+
+# Second task
+
+def print_body(height, arm):
+    print('    XX')
+    print('#' + arm * 4 + 'XX' + arm *4 +'#')
+    for i in range(height):
+        print('    XXXX')
+
+# Third task 
+
+def reverse_shoe(shoe_string):
+    new_shoe_string = ''
+    for i in shoe_string[::-1]:
+        new_shoe_string = new_shoe_string + i
+        
+    return new_shoe_string
+
+# Fourth task
+
+def print_legs(height, shoe):
+    rreverse_shoe = reverse_shoe(shoe)
+    print('    ====')
+    for i in range(height):
+        print('   ||  ||')
+    print(f' {shoe}  {rreverse_shoe}')
